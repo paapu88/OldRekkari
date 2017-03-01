327a2ab62300904afad69e2f54dba272708ec5a6
paapu68 <markus.kaukonen@iki.fi>
2017-03-01 16:34:48 +0200

    testing

--- faceDetection.py
+++ faceDetection.py
@@ -16,6 +16,7 @@ for (x,y,w,h) in faces:
     for (ex,ey,ew,eh) in eyes:
         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
+
 cv2.imshow('img',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
